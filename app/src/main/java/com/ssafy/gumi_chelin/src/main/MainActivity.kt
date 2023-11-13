e1f230c4cadd66051819a244262f33274a13f450
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-11-14 02:45:38 +0900

    Setting package arch & Add login, main activity

---
+++ app/src/main/java/com/ssafy/gumi_chelin/src/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.gumi_chelin.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.gumi_chelin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
