e55acf7c7ccbc1dbd0c1d3fc1c21ce799b89e4c5
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-11-15 03:17:59 +0900

    Update login activity, fragment

--- app/src/main/java/com/ssafy/gumichelin/src/login/LoginActivity.kt
+++ app/src/main/java/com/ssafy/gumichelin/src/login/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.ssafy.gumichelin.src.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.gumichelin.R
+import com.ssafy.gumichelin.config.BaseActivity
+import com.ssafy.gumichelin.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout_login, LoginFragment())
+            .commit()
     }
 }
