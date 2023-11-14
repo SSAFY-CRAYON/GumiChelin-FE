01d422d1a2f68dd1cd9ce7d57cca25fc24564f3c
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-11-15 01:58:49 +0900

    Resetting package arch & Add login, main activity

--- app/src/main/java/com/ssafy/gumichelin/src/login/LoginActivity.kt
+++ app/src/main/java/com/ssafy/gumichelin/src/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.ssafy.gumichelin
+package com.ssafy.gumichelin.src.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssafy.gumichelin.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/ssafy/gumichelin/src/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.gumichelin.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.gumichelin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
