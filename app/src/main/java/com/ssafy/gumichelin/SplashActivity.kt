169e5b73566307f1c27b0055025e76d486871250
joongwon <wnddnjs823@ajou.ac.kr>
2023-11-15 17:01:36 +0900

    Add 앱 로고, 스플래시 화면

---
+++ app/src/main/java/com/ssafy/gumichelin/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.ssafy.gumichelin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ssafy.gumichelin.src.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed( {
+
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, 500)
+    }
+}
--- app/src/main/java/com/ssafy/gumichelin/src/login/Join1Fragment.kt
+++ app/src/main/java/com/ssafy/gumichelin/src/login/Join1Fragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [JoinFragment.newInstance] factory method to
+ * Use the [Join1Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class JoinFragment : Fragment() {
+class Join1Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class JoinFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_join, container, false)
+        return inflater.inflate(R.layout.fragment_join1, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class JoinFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            JoinFragment().apply {
+            Join1Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
